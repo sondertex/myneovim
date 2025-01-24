fba6f8cc75ca744d642a64932faaed071f3d5d56
serenity <HilbertYu@outlook.com>
2025-01-24 17:30:06 +0800

    A latest version

--- init.lua
+++ init.lua
@@ -7,18 +7,26 @@ require("config.lazy")
 ------------------
 if vim.g.neovide then
   vim.opt.guifont = { "FiraCode Nerd Font", ":h13" } -- 字体和大小
-  vim.g.neovide_transparency = 0.6              -- 背景透明度
+  vim.g.neovide_transparency = 0.8              -- 背景透明度
   vim.g.neovide_cursor_vfx_mode = "railgun"         -- 光标特效
 end
 
-vim.opt.swapfile = false
-vim.opt.writebackup = false
-
-vim.cmd("set wrap")
--- 启用语法高亮
-vim.cmd("syntax on")
+vim.opt.swapfile = false 
+vim.opt.writebackup = false 
 
 
+vim.cmd("set wrap")  -- 按行宽换行
+vim.cmd("syntax on")  -- 启用语法高亮
+
+vim.api.nvim_set_hl(0, "Conceal", { fg = "#85D3F2" }) -- 代码高亮
+
+vim.g.neovide_window_blurred = true  -- 毛玻璃效果
+
+------------------
+
+-- 自动保存
+
+------------------
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
@@ -28,16 +36,19 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
+------------------
+
+-- lualine
+
+------------------
+
 require'lualine'.setup {
     options = {
     theme = 'sonokai'
     }
     }
 
-vim.api.nvim_set_hl(0, "Conceal", { fg = "#85D3F2" })
 
-vim.g.neovide_show_border = true --边框
-vim.g.neovide_window_blurred = true  --毛玻璃效果
 
 
 
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,39 +3,36 @@ local map = vim.keymap.set
 local hop = require("hop")
 local directions = require("hop.hint").HintDirection
 local positions = require("hop.hint").HintPosition
--- leader leader w
-map({ "n", "v" }, "<leader><leader>a", function()
+-- leader a: 向下跳转到某个单词词头
+map({ "n", "v" }, "<leader>a", function()
   hop.hint_words({ direction = directions.AFTER_CURSOR })
 end, { desc = "Go to next any begining of words" })
--- leader leader e
-map({ "n", "v" }, "<leader><leader>s", function()
+-- leader s:向下跳转到某个单词词尾
+map({ "n", "v" }, "<leader>s", function()
   hop.hint_words({ direction = directions.AFTER_CURSOR, hint_position = positions.END })
 end, { desc = "Go to next any end of words" })
--- leader leader b
-map({ "n", "v" }, "<leader><leader>q", function()
+-- leader q:向上跳转到某个单词词头
+map({ "n", "v" }, "<leader>q", function()
   hop.hint_words({ direction = directions.BEFORE_CURSOR })
 end, { desc = "Go to previous any begining of words" })
--- leader leader v
-map({ "n", "v" }, "<leader><leader>w", function()
+-- leader w:向上跳转到某个单词词尾
+map({ "n", "v" }, "<leader>w", function()
   hop.hint_words({ direction = directions.BEFORE_CURSOR, hint_position = positions.END })
 end, { desc = "Go to previous any end of words" })
--- leader leader l
 
--- leader leader l
-map({ "n", "v" }, "<leader><leader>k", function()
-  hop.hint_camel_case({ direction = directions.AFTER_CURSOR })
-end, { desc = "Go to next any begining of words considering camel case." })
--- leader leader h
-map({ "n", "v" }, "<leader><leader>j", function()
-  hop.hint_camel_case({ direction = directions.BEFORE_CURSOR })
-end, { desc = "Go to next any begining of words considering camel case." }) 
 
--- leader leader j
-map({ "n", "v" }, "<leader><leader>i", function()
-  hop.hint_lines({ direction = directions.AFTER_CURSOR })
-end, { desc = "Go to line below" })
--- leader leader k
-map({ "n", "v" }, "<leader><leader>u", function()
+-- leader j:向上跳转到某行开头(没什么大用)
+map({ "n", "v" }, "<leader>j", function()
   hop.hint_lines({ direction = directions.BEFORE_CURSOR })
-end, { desc = "Go to line above" })
+end, { desc = "Go to line end of the previous line" })
+-- leader k:向下跳转到某行开头(没什么大用)
+map({ "n", "v" }, "<leader>", function()
+  local current_line = vim.fn.line('.')
+  hop.hint_lines({ direction = directions.AFTER_CURSOR, current_line = current_line + 1 })
+end, { desc = "Go to line end of the next line" })
+
+-- leader r: 跳转回跳转前的位置
+vim.keymap.set('n', '<leader>r', '<C-o>', { desc = 'Jump back (like Ctrl+o)' })
+
+
 
