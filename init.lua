56985b0d73994dc5e95228678779cd0075273898
serenity <HilbertYu@outlook.com>
2025-02-02 22:43:21 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -5,11 +5,11 @@ require("config.lazy")
 -- neovide
 
 ------------------
-
 if vim.g.neovide then
   vim.opt.guifont = { "0xProto Nerd Font", ":h12" } -- 字体设置
   vim.g.neovide_cursor_vfx_duration = 0.05  -- 设置更短的持续时间来加快动画
   vim.g.neovide_fully_buffered = true  -- 开启完全缓冲
+  vim.g.neovide_sync = true -- 输入同步
 end
 
 
@@ -17,11 +17,6 @@ vim.o.wrap = true        -- 启用换行
 vim.o.linebreak = true   -- 避免单词中间断行
 vim.cmd("syntax on")  -- 启用语法高亮
 vim.cmd [[highlight Conceal guifg=#a6e3a1]]   -- 数学环境的显示公式颜色
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.fn.system("open -n -a Neovide --args --no-titlebar")
-  end,
-})
 ------------------
 
 -- lualine
@@ -34,7 +29,6 @@ require('lualine').setup({
     lualine_c = {''}, -- 空白
   }
 })
-
 
 
 
